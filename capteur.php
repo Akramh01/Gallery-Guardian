d4ac2f92bdbced4e5bd7b81cd41bebf6606c116b
Akramh01 <akram.hamlat@univ-tlse3.fr>
2024-05-14 18:56:12 +0200

    test

---
+++ capteur.php
@@ -0,0 +1,15 @@
+<?php
+
+include 'bd.php';
+
+$ch = curl_init("http://192.168.4.1:8080/json.htm?type=command&param=getdevices&rid=12");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$data = curl_exec($ch);
+$json = json_decode($data, true);
+$devices = $json['result'];
+echo $devices[0];
+// $device = $devices[0];
+// echo "temperature actuelle : ".$device['Temp'];
+curl_close($ch);
+
+?>
