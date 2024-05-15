add99bac19f7e529fb3d1ccd11cf1e16643a4a39
WidadeHas <widade.hasnaoui@gmail.com>
2024-05-15 09:39:40 +0200

    modif5

--- fichierphp.php
+++ fichierphp.php
@@ -1,5 +1,23 @@
 
-include 'bd.php';
+<?php
+
+$servername = "localhost"; //192.168.4.1 pour le raspberry
+$username = "root"; //username dans le raspberry sera different
+$pass = ""; //password dans le raspberry sera different
+
+//Connexion à la base de donnée
+try{
+            
+    $bdd = new PDO("mysql:host=$servername;dbname=15maigallery", $username, $pass);
+    
+    //connexion a la base de donnees, dbname pour le nom de la base de donnée que je n'ai pas encore mit
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //spécifier le type d'erreur
+    echo "Connected successfully";
+}catch(PDOException $e){
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
