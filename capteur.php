34e0cbb8e644db5496ae7b4c873410be7b3aee40
Akramh01 <akram.hamlat@univ-tlse3.fr>
2024-05-15 10:09:08 +0200

    #

--- capteur.php
+++ capteur.php
@@ -2,7 +2,7 @@
 
 include 'bd.php';
 
-$ch = curl_init("http://192.168.4.1:8080/json.htm?type=command&param=getdevices&rid=12");
+$ch = curl_init("http://192.168.4.1:8080/json.htm?type=command&param=getdevices&rid=34");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $data = curl_exec($ch);
 $json = json_decode($data, true);
