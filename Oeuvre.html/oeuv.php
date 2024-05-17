07c7b03f22bbb5cab74078b69185bd203b353f34
SBouaz <salmabouazzaoui90@gmail.com>
2024-05-17 16:00:18 +0200

    la fin

--- Oeuvre.html/oeuv.php
+++ Oeuvre.html/oeuv.php
@@ -1,42 +1,46 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Gallery Guardian</title>
-        <link rel="stylesheet" type="text/css" href="oeuv.css">
-       
-    </head>
-    <body>
-        <label class = "navbar">
-            <input type="checkbox" class = "toggler">
-            <div class="toggle">
-              <span class="top_line common"></span>
-              <span class="middle_line common"></span>
-              <span class="bottom_line common"></span>
-            </div>
-      
-            <div class = "slide">
-              <h1>MENU</h1>
-              <ul>
-                <li><a href = '#'><i class = "fas fa-user"></i> Profile </a></li>
-                <li><a href = '#'><i class = "far fa-address-book"></i> Télécommande </a></li>
-                <li><a href = '#'><i class = "fas fa-tv"></i> Oeuvre d'art </a></li>
-                <li><a href = '#'><i class = "fas fa-comments"></i> Events </a></li>
-                <li><a href = 'login.html'><i class = "fas fa-user"></i> Se connecter </a></li>
-      
-              </ul>
-            </div>
-        </label>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"> <!-- Encodage des caractères en UTF-8 -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Configuration de la mise en page pour les appareils mobiles -->
+    <title>Notre site internet</title> <!-- Titre de la page -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"> <!-- Lien vers les icônes Font Awesome -->
+    <link rel="stylesheet" type="text/css" href="oeuv.css"> <!-- Lien vers le fichier CSS externe -->
+</head> 
+
+<body>
+    <!-- Menu toggle avec checkbox pour afficher/masquer le menu -->
+    <label>
+      <input type="checkbox">
+      <div class="toggle">
+        <span class="top_line common"></span>
+        <span class="middle_line common"></span>
+        <span class="bottom_line common"></span>
+      </div>
+
+      <!-- Menu slide -->
+      <div class="slide">
+        <h1>MENU</h1>
+        <ul>
+          <li><a href="#"><i class="fas fa-user"></i> Profile </a></li>
+          <li><a href="#"><i class="far fa-address-book"></i> Télécommande </a></li>
+          <li><a href="#"><i class="fas fa-tv"></i> Oeuvre d'art </a></li>
+          <li><a href="#"><i class="fas fa-comments"></i> Events </a></li>
+        </ul>
+      </div>
+    </label>
 
         <div class="header">
             <img src="Logo.png" alt="Logo" width="50" height="50">
             <h1>GalleryGuardian</h1>
          
-        </div>
-  <div class="container">
-    <div class="table-container">
-      <h4>Localisation des oeuvres </h4>
-      <table border="1"> 
+        </div >
+        <div class="content">
+  <<div class="main-content">
+        <div class="table-container">
+            <h4>Localisation des oeuvres</h4>
+            <table class="center-table" border="1">
+      <
         <thead>
           <tr>
             <th>Num oeuvre</th>
@@ -77,7 +81,7 @@
     <h2>Plan de musée</h2>
     <img src="R.jpeg" alt="Plan du musée">
   </div>
-
+        </div>
   <footer class =footer>
     <p>&copy; 2024 Mon Site Web. Tous droits réservés.</p>
   </footer>
